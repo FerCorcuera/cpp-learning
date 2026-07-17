ab4a0994d912f1c131d33a94cfddb89d5172461a
Fernando Corcuera <188613791+FerCorcuera@users.noreply.github.com>
2026-07-16 23:08:49 -0500

    hello world with cpp

---
+++ empty.cpp
@@ -0,0 +1,10 @@
+// This is my first C++ code
+// The main function is special, returns an int if everyhing has worked great
+// You can have only one main function perfile!
+int main()
+
+{
+
+}
+// with this and the command we are not only creating the compiled file
+// But also behidn the scenes the linker has been set!
---
+++ hello_println.cpp
@@ -0,0 +1,13 @@
+#include <print>
+
+int main()
+{
+  std::print("Hello, world!"); //in C++ every statement needs to end with semicolons
+}
+// lets think about std as a folder that has a set of functions
+// It is ipmortant because we can name a function as any other,
+// But only if they live in different "folders" (namespace)
+// We can tink about the :: symbol as "take a look inside"
+
+//indentation here is only for hvaing a clean code, but the compiler
+//cares about the semilocons!
