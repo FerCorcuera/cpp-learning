151407a35c155474c0b7eb14927568d0f1b07a76
Fernando Corcuera <188613791+FerCorcuera@users.noreply.github.com>
2026-07-20 02:02:48 -0500

    chapter_02: detecting input problems done; ' '

--- fundamentals/chapter_02/input_experiment.cpp
+++ fundamentals/chapter_02/input_experiment.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-
+#include <limits>
 // eof() is a member function in the input stream, so it is part from cin
 // eof stands for "End of file"
 // Member functions need to be called with dot operator
-// So now we are calling an instance function, not a class function (where we used ::)
+// So now we are calling an instance function, not a class instance (where we used ::)
 
-if(!std::cin.eof()) // so this could be read as
-                    // check if we have reached the end of the stream
+
+int main()
 {
-  std::cout << "Unused input\n"
+  const double largest = std::numeric_limits<double>::max();
+  std::cout << "Please enter a number up to " <<largest << ".\n>";
+  double number {};
+  std::cin >> number;
+  std::cout << number << '\n';
+  if(!std::cin.eof() && std::cin.peek()!='\n')
+    // .peek() observes the next character that was left in the buffer
+    // both statements could be read as:
+    // 1. we have not reached the end of the file
+    // 2. The next unread character is not a new line
+  {
+    std::cout << "Unused input\n";
+  }
 }
+
